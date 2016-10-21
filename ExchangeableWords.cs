b7a1b84d9365042919be39dc2910a860953d1b77
dimo009 <d_chavdarov@hotmail.com>
2016-10-21 18:21:01 +0300

    Add files via upload

---
+++ ExchangeableWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Magic_exchangeable_words
+{
+    class ExchangeableWords
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            char[] firstArr = input[0].ToCharArray();
+            char[] secondArr = input[1].ToCharArray();
+
+            var firstList = new List<char>();
+            var secondList = new List<char>();
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (!firstList.Any(x => x == firstArr[i]))
+                {
+                    firstList.Add(firstArr[i]);
+                }
+            }
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                if (!secondList.Any(x => x == secondArr[i]))
+                {
+                    secondList.Add(secondArr[i]);
+                }
+            }
+            if (firstList.Count == secondList.Count)
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
---
+++ SumOfBigNumbers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SumOfBigNumbersHW
+{
+    class SumOfBigNumbers
+    {
+        static void Main(string[] args)
+        {
+            string term1 = Console.ReadLine();
+
+            string term2 = Console.ReadLine();
+
+            System.Text.StringBuilder sum = new StringBuilder();
+
+            int n1 = term1.Length;
+
+            int n2 = term2.Length;
+
+            int carry = 0;
+
+            int n = (n1 > n2) ? n1 : n2;
+
+            if (n1 > n2)
+
+                term2 = term2.PadLeft(n1, '0');
+
+            else
+
+                term1 = term1.PadLeft(n2, '0');
+
+            for (int i = n - 1; i >= 0; i--)
+
+            {
+
+                int value = (carry + term1[i] - 48 + term2[i] - 48) % 10;
+
+                sum.Append(value);
+
+                carry = (carry + term1[i] - 48 + term2[i] - 48) / 10;
+
+            }
+
+            char[] c = sum.ToString().ToCharArray();
+
+            Array.Reverse(c);
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
