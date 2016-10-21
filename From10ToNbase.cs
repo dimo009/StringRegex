8a70040c56012b082fcd452e51651fddd51ce9e0
dimo009 <d_chavdarov@hotmail.com>
2016-10-21 18:12:51 +0300

    Add files via upload

---
+++ From10ToNbase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Homework_Convert_from_10_base_to_N_base
+{
+    class From10ToNbase
+    {
+        static void Main(string[] args)
+        {
+
+             
+
+            
+            List<BigInteger> numbers = new List<BigInteger>();
+           
+            numbers = Console.ReadLine().Split().Select(BigInteger.Parse).ToList();
+            int n = (int)numbers[0];
+            BigInteger number = numbers[1];
+            BigInteger remainder;
+            string result = null;
+
+            if (n>=2&&n<=10)
+            {
+                while (number>0)
+                {
+                    remainder = number % n;
+                    number /= n;
+
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
